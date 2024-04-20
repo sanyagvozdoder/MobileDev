85e3f1e202b912d9f3153d16d240d0fe7dd4eac7
sanyagvozdoder <alexshadrov1712@gmail.com>
2024-04-20 15:19:15 +0700

    First Commit

--- app/src/main/java/com/example/mobiledev/MainActivity.kt
+++ app/src/main/java/com/example/mobiledev/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.mobiledev
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mobiledev.ui.theme.MobileDevTheme
@@ -16,31 +18,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MobileDevTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            Column {
+                Text(text = "Hello world", modifier = Modifier.align(Alignment.CenterHorizontally))
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MobileDevTheme {
-        Greeting("Android")
     }
 }
