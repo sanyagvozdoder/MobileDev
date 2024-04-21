323e5b081a260a9a599db32869d611cd6ebfc805
sanyagvozdoder <alexshadrov1712@gmail.com>
2024-04-22 02:10:32 +0700

    architecture template and navigation

--- app/src/main/java/com/example/mobiledev/presentation/mainactivity/MainActivity.kt
+++ app/src/main/java/com/example/mobiledev/presentation/mainactivity/MainActivity.kt
@@ -1,19 +1,24 @@
-package com.example.mobiledev
+package com.example.mobiledev.presentation.mainactivity
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.example.mobiledev.presentation.navgraph.NavGraph
 
 class MainActivity : ComponentActivity() {
+
+    private val mainViewModel by viewModels<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column {
-                Text(text = "Hello world", modifier = Modifier.align(Alignment.CenterHorizontally))
+            Box() {
+                NavGraph(startDestination = mainViewModel.startdestination)
             }
         }
     }
