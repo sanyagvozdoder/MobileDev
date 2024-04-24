796d15aa3923b71c582ce7d0b7d985d9a7e29d37
sanyagvozdoder <alexshadrov1712@gmail.com>
2024-04-24 21:30:32 +0700

    работает база для создания алгосов(update)

--- app/src/main/java/com/example/mobiledev/presentation/algoritms/Scaling.kt
+++ app/src/main/java/com/example/mobiledev/presentation/algoritms/Scaling.kt
@@ -1,9 +1,13 @@
 package com.example.mobiledev.presentation.algoritms
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Color
 import android.net.Uri
 import android.os.Environment
-import android.util.Base64
+import com.example.mobiledev.presentation.algoritms.util.Rgb
 import com.example.mobiledev.presentation.editorscreen.EditorScreenViewModel
+import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileOutputStream
 import kotlin.io.encoding.ExperimentalEncodingApi
@@ -11,15 +15,35 @@ import kotlin.io.encoding.ExperimentalEncodingApi
 @OptIn(ExperimentalEncodingApi::class)
 fun Scaling(img:ByteArray?, viewModelInstance:EditorScreenViewModel):Unit{
 
-    var newImg:ByteArray = ByteArray(img?.size?:0)
+    var bitmap = BitmapFactory.decodeByteArray(img,0, img?.size?:0)
+    var mutableBitmap = bitmap.copy(Bitmap.Config.ARGB_8888,true)
 
-    var ind = 0
+    var arrRgb = List<List<Rgb>>(bitmap.height){List<Rgb>(bitmap.width){Rgb(0,0,0,0)}}
 
-    img?.forEach {byte->
-        var outputByte = byte
-        newImg.set(ind,outputByte)
-        ind++
+    for (i in 0..<bitmap.height){
+        for (j in 0..<bitmap.width){
+            val pixel = bitmap.getPixel(j,i)
+            arrRgb[i][j].red = Color.red(pixel)
+            arrRgb[i][j].green = Color.green(pixel)
+            arrRgb[i][j].blue = Color.blue(pixel)
+            arrRgb[i][j].alpha = Color.alpha(pixel)
+
+            arrRgb[i][j].red = 0
+            arrRgb[i][j].green = 0
+            arrRgb[i][j].blue = 0
+            arrRgb[i][j].alpha = 255
+
+            mutableBitmap.setPixel(
+                j,
+                i,
+                Color.argb(arrRgb[i][j].alpha,arrRgb[i][j].red,arrRgb[i][j].green,arrRgb[i][j].blue)
+            )
+        }
     }
+
+    val stream = ByteArrayOutputStream()
+    mutableBitmap.compress(Bitmap.CompressFormat.JPEG,90,stream)
+    val newImg = stream.toByteArray()
 
     val downloadsDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
 
